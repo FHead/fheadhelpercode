b600adbf48aad9c297d6307c12e332826f33f504
chen.yi.first <chen.yi.first@c3ce6b84-1c9d-a277-69f4-410ed7fba8ba>
2011-08-25 19:46:17 +0000

    PlotHelper2 update

--- trunk/RootMacros/SetStyle.h
+++ trunk/RootMacros/SetStyle.h
@@ -16,6 +16,66 @@ void SetStyle()
    gStyle->SetFillColor(0);
 
    gStyle->SetPaperSize(20,26);
+   // gStyle->SetPadTopMargin(0.05);
+   // gStyle->SetPadRightMargin(0.05);
+   // gStyle->SetPadBottomMargin(0.16);
+   // gStyle->SetPadLeftMargin(0.12);
+
+   // gStyle->SetTextFont(42);
+   // gStyle->SetTextSize(0.08);
+   gStyle->SetLabelFont(42,"x");
+   gStyle->SetLabelFont(42,"y");
+   gStyle->SetLabelFont(42,"z");
+   gStyle->SetLabelSize(0.035,"x");
+   gStyle->SetTitleSize(0.035,"x");
+   gStyle->SetLabelSize(0.035,"y");
+   gStyle->SetTitleSize(0.035,"y");
+   gStyle->SetLabelSize(0.035,"z");
+   gStyle->SetTitleSize(0.035,"z");
+
+   gStyle->SetMarkerStyle(20);
+   gStyle->SetHistLineWidth(1);
+   // gStyle->SetHistLineWidth(1.85);
+   // gStyle->SetHistLineWidth(3.85);
+   gStyle->SetLineStyleString(2,"[12 12]"); // postscript dashes
+
+   gStyle->SetErrorX(0.001);
+
+   // gStyle->SetOptTitle(0);
+   gStyle->SetOptStat(0);
+   gStyle->SetOptFit(0);
+
+   gStyle->SetPadTickX(1);
+   gStyle->SetPadTickY(1);
+
+   gStyle->SetOptStat(1100);
+   gStyle->SetOptStat(1);
+   gStyle->SetOptFit(1111);
+   gStyle->SetPadTickX(1);
+   gStyle->SetPadTickY(1);
+
+   const Int_t NRGBs = 5;
+   const Int_t NCont = 255;
+
+   Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
+   Double_t red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
+   Double_t green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
+   Double_t blue[NRGBs]  = { 0.51, 1.00, 0.12, 0.00, 0.00 };
+   TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
+   gStyle->SetNumberContours(NCont);
+}
+
+void SetStyleOriginal()
+{
+   gStyle->SetFrameBorderMode(0);
+   gStyle->SetCanvasBorderMode(0);
+   gStyle->SetPadBorderMode(0);
+   gStyle->SetPadColor(0);
+   gStyle->SetCanvasColor(0);
+   gStyle->SetStatColor(0);
+   gStyle->SetFillColor(0);
+
+   gStyle->SetPaperSize(20,26);
    gStyle->SetPadTopMargin(0.05);
    gStyle->SetPadRightMargin(0.05);
    gStyle->SetPadBottomMargin(0.16);
