f2ca92084e602b1d4b6f8b245a5e5a83385ee220
chen.yi.first <chen.yi.first@c3ce6b84-1c9d-a277-69f4-410ed7fba8ba>
2011-09-01 15:52:41 +0000

    New DateConverter

--- trunk/Program/DateConverterNew/DateConverter.cpp
+++ trunk/Program/DateConverterNew/DateConverter.cpp
@@ -16,21 +16,22 @@ time_t ToDate(string str);
 //-------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
-   // no parameter = print today && (1111)
-   // with parameter = conversion from (1111) to 20061023 or vise versa
+   // no parameter = print today && (1111) && usage
+   // with parameter = conversion from ((0) 1111) to 20061023 or vise versa
    //    depends on the format
-   
-   // time_t day1111 = Get1111();
-   // cout << "(" << PMDateString(day1111) << ") = " << DateString(day1111) << endl;   
-
-   // time_t today = time(NULL);
-   // cout << "(" << PMDateString(today) << ") = " << DateString(today) << endl;
    
    if(argc == 1)
    {
-      cout << "Usage: exe 2532" << endl;
-      cout << "       exe 20070501" << endl;
-      cout << "Note the date format is assumed to be YYYYMMDD" << endl;
+      cout << "Usage: " << argv[0] << " 2532" << endl;
+      cout << "       " << argv[0] << " 0,2532" << endl;
+      cout << "       " << argv[0] << " 20070501" << endl;
+      cout << "Note the date format is assumed to be YYYYMMDD" << endl << endl;
+   
+      time_t day1111 = Get1111();
+      cout << "[" << PMDateString(day1111) << "] = " << DateString(day1111) << endl;   
+
+      time_t today = time(NULL);
+      cout << "[" << PMDateString(today) << "] = " << DateString(today) << endl;
    }
    else
    {
@@ -41,12 +42,14 @@ int main(int argc, char *argv[])
          string arg = argv[i];
          if(arg.size() == 4)
             inputdate = PMToDate(arg);
+         else if(arg.size() > 4 && arg[arg.size()-1-4] == ',')
+            inputdate = PMToDate(arg);
          else if(arg.size() == 8)
             inputdate = ToDate(arg);
          else
             continue;
          
-         cout << "(" << PMDateString(inputdate) << ") = " << DateString(inputdate) << endl;   
+         cout << "[" << PMDateString(inputdate) << "] = " << DateString(inputdate) << endl;   
       }
    }
    
@@ -78,10 +81,21 @@ string PMDateString(time_t date)
    stringstream str;
 
    time_t day1111 = Get1111();
-   double diffseconds = difftime(date, day1111) + 3903;
+   long long diffseconds = difftime(date, day1111) + 3532;
+
+   if(diffseconds < 0 || diffseconds >= 2401 * 60 * 60 * 24)
+   {
+      int period = diffseconds / 2401 / 60 / 60 / 24;
+      if(diffseconds < 0)
+         period = period - 1;
+      diffseconds = diffseconds - period * 2401 * 60 * 60 * 24;
+
+      str << "(" << period << ") ";
+   }
+   
    long diffdays = (long)(diffseconds / 60 / 60 / 24);
 
-   str << diffdays / 343 + 1
+   str << (diffdays / 343) % 7 + 1
       << (diffdays / 49) % 7 + 1
       << (diffdays / 7) % 7 + 1
       << diffdays % 7 + 1;
@@ -94,7 +108,7 @@ time_t Get1111()
    tm day1711info;
    day1711info.tm_sec = 1;
    day1711info.tm_min = 0;
-   day1711info.tm_hour = 1;
+   day1711info.tm_hour = 0;
    day1711info.tm_mon = 8 - 1;   // August
    day1711info.tm_mday = 14;   // 14th
    day1711info.tm_year = 106;   // 2006 - 1900 = 106
@@ -109,14 +123,25 @@ time_t PMToDate(string str)
 {
    time_t day1111 = Get1111();
 
+   int period = 0;
+
+   if(str.size() > 4)
+   {
+      stringstream periodstr(str.substr(0, str.size() - 5));
+      periodstr >> period;
+      str = str.substr(str.size() - 4, 4);
+   }
+
    long daydiff = (int)(str[0] - '1') * 343
       + (int)(str[1] - '1') * 49
       + (int)(str[2] - '1') * 7
       + (int)(str[3] - '1') * 1;
+   daydiff = daydiff + 2401 * period;
+
    // cout << "daydiff " << daydiff << endl;
 
-   time_t day = day1111 + daydiff * 24 * 60 * 60 + 1352;
-
+   time_t day = day1111 + daydiff * 24 * 60 * 60 + 1234;
+   
    return day;
 }
 //-------------------------------------------------------------------------
