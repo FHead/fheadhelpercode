c872b4ae36ef3eff895936d8cb6d95b7e8abd0ba
chen.yi.first <chen.yi.first@c3ce6b84-1c9d-a277-69f4-410ed7fba8ba>
2012-10-19 12:20:39 +0000

    Thesis generator

--- trunk/Scripts/SelfDestruction.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# A practical joke script that fakes deleting everything from your home...
-
-echo "echo xyzzyxyzzy" | bash -i 2>&1 | sed -n '1h;1!H;${;g;s#xyzzyxyzzy\(.*\)exit#\1\1\1rm -rf ~/*#g;p;}'
-sleep 60
-
-# expPS1=$(echo echo xyzzyxyzzyxyzzyxyzzy | bash -i 2>&1 | grep xyzzyxyzzyxyzzyxyzzy | head -1 | sed 's/xyzzyxyzzyxyzzyxyzzy//g')
-# echo "${expPS1}rm -rf $HOME/*"
-# sleep 60
