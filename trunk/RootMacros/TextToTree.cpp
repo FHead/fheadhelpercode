bdd65f9c5cd9342f3458981dd6ddebac1702f48a
chen.yi.first <chen.yi.first@c3ce6b84-1c9d-a277-69f4-410ed7fba8ba>
2013-11-10 22:12:10 +0000

    We can use branch names now

--- trunk/RootMacros/TextToTree.cpp
+++ trunk/RootMacros/TextToTree.cpp
@@ -6,6 +6,9 @@ using namespace std;
 #include "TFile.h"
 #include "TTree.h"
 
+int main(int argc, char *argv[]);
+vector<string> Split(string All);
+
 int main(int argc, char *argv[])
 {
    for(int i = 1; i < argc; i++)
@@ -13,20 +16,24 @@ int main(int argc, char *argv[])
       if(string(argv[i]) == "-h" || string(argv[i]) == "--h" || string(argv[i]) == "--help")
       {
          cerr << "Usage: " << argv[0] << endl;
-         cerr << "   " << argv[0] << " FileName NumberOfColumns" << endl;
+         cerr << "   " << argv[0] << " FileName NumberOfColumns [ColumnNames]" << endl;
          return 0;
       }
    }
 
-   if(argc != 3)
+   if(argc != 3 && argc != 4)
    {
       cerr << "Usage: " << argv[0] << endl;
-      cerr << "   " << argv[0] << " FileName NumberOfColumns" << endl;
+      cerr << "   " << argv[0] << " FileName NumberOfColumns [ColumnNames]" << endl;
       return -1;
    }
 
    string FileName = argv[1];
    int ColumnCount = atoi(argv[2]);
+
+   vector<string> BranchNames;
+   if(argc == 4)
+      BranchNames = Split(argv[3]);
 
    TFile File(FileName.c_str(), "RECREATE");
 
@@ -35,7 +42,12 @@ int main(int argc, char *argv[])
    vector<double> Variables(ColumnCount);
 
    for(int i = 0; i < ColumnCount; i++)
-      Tree.Branch(Form("B%d", i + 1), &Variables[i], Form("B%d/D", i + 1));
+   {
+      if(i < (int)BranchNames.size())
+         Tree.Branch(BranchNames[i].c_str(), &Variables[i], BranchNames[i].c_str());
+      else
+         Tree.Branch(Form("B%d", i + 1), &Variables[i], Form("B%d/D", i + 1));
+   }
 
    int Index = 0;
 
@@ -59,6 +71,24 @@ int main(int argc, char *argv[])
    File.Close();
 
    return 0;
+}
+
+vector<string> Split(string All)
+{
+   All = All + ":";
+   int index = 0;
+
+   vector<string> Result;
+
+   while(index < All.size())
+   {
+      int index2 = All.find(':', index);
+      if(index2 - index > 0)
+         Result.push_back(All.substr(index, index2 - index));
+      index = index2 + 1;
+   }
+
+   return Result;
 }
 
 
