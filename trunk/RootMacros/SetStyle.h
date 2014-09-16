c3cb7ab18d0f9422f77719b64079bee4ca59df62
chen.yi.first <chen.yi.first@c3ce6b84-1c9d-a277-69f4-410ed7fba8ba>
2014-09-16 17:32:44 +0000

    Style

--- trunk/RootMacros/SetStyle.h
+++ trunk/RootMacros/SetStyle.h
@@ -7,12 +7,82 @@
 #include "TColor.h"
 #include "TLatex.h"
 
+void SetThesisStyle();
 void SetStyle();
 void SetStyleOriginal();
 void SetTDRStyle();
 void AddCMSPreliminary(double X = 0.15, double Y = 0.875, double Luminosity = -1, double FontSize = 0.03);
 void AddCMS(double X = 0.15, double Y = 0.875, double Luminosity = -1, double FontSize = 0.03);
 void AddCMSSimulation(double X = 0.15, double Y = 0.875, double Luminosity = -1, double FontSize = 0.03);
+
+void SetThesisStyle()
+{
+   gStyle->SetFrameBorderMode(0);
+   gStyle->SetCanvasBorderMode(0);
+   gStyle->SetPadBorderMode(0);
+   gStyle->SetPadColor(0);
+   gStyle->SetCanvasColor(0);
+   gStyle->SetStatColor(0);
+   gStyle->SetFillColor(0);
+
+   gStyle->SetPaperSize(20,26);
+   gStyle->SetCanvasDefH(1024);
+   gStyle->SetCanvasDefW(1024 * 1.05);
+   gStyle->SetPadTopMargin(0.1);
+   gStyle->SetPadRightMargin(0.15 / 1.05);
+   gStyle->SetPadBottomMargin(0.1);
+   gStyle->SetPadLeftMargin(0.1);
+
+   // gStyle->SetTextFont(42);
+   // gStyle->SetTextSize(0.08);
+   gStyle->SetLabelFont(42,"x");
+   gStyle->SetLabelFont(42,"y");
+   gStyle->SetLabelFont(42,"z");
+   gStyle->SetLabelSize(0.035,"x");
+   gStyle->SetTitleSize(0.035,"x");
+   gStyle->SetLabelSize(0.035,"y");
+   gStyle->SetTitleSize(0.035,"y");
+   gStyle->SetLabelSize(0.035,"z");
+   gStyle->SetTitleSize(0.035,"z");
+   gStyle->SetTitleOffset(1.10,"x");
+   gStyle->SetTitleOffset(1.20,"y");
+   gStyle->SetTitleOffset(1.28,"z");
+
+   gStyle->SetMarkerStyle(20);
+   gStyle->SetHistLineWidth(1);
+   // gStyle->SetHistLineWidth(1.85);
+   // gStyle->SetHistLineWidth(3.85);
+   gStyle->SetLineStyleString(2,"[12 12]"); // postscript dashes
+
+   gStyle->SetErrorX(0.001);
+
+   // gStyle->SetOptTitle(0);
+   gStyle->SetOptStat(0);
+   gStyle->SetOptFit(0);
+
+   gStyle->SetPadTickX(1);
+   gStyle->SetPadTickY(1);
+
+   gStyle->SetOptStat(1100);
+   gStyle->SetOptStat(1);
+   gStyle->SetOptFit(1111);
+   gStyle->SetPadTickX(1);
+   gStyle->SetPadTickY(1);
+
+   gStyle->SetLegendFont(42);
+   gStyle->SetLegendBorderSize(0);
+   gStyle->SetLegendFillColor(0);
+
+   const Int_t NRGBs = 5;
+   const Int_t NCont = 99;
+
+   Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
+   Double_t red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
+   Double_t green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
+   Double_t blue[NRGBs]  = { 0.51, 1.00, 0.12, 0.00, 0.00 };
+   TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
+   gStyle->SetNumberContours(NCont);
+}
 
 void SetStyle()
 {
@@ -64,7 +134,7 @@ void SetStyle()
    gStyle->SetPadTickY(1);
 
    const Int_t NRGBs = 5;
-   const Int_t NCont = 255;
+   const Int_t NCont = 99;
 
    Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
    Double_t red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
@@ -125,7 +195,7 @@ void SetStyleOriginal()
    gStyle->SetPadTickY(1);
 
    const Int_t NRGBs = 5;
-   const Int_t NCont = 255;
+   const Int_t NCont = 99;
 
    Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
    Double_t red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
