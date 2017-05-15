48774ac8413f5c0a2cd8b5482f4a1e160daa1096
Yi Chen <chen.yi.first@gmail.com>
2017-05-15 10:38:23 +0200

    New thumb style

--- RootMacros/SetStyle.h
+++ RootMacros/SetStyle.h
@@ -13,6 +13,7 @@ void SetThesisStyle();
 void SetThesisBigStyle();
 void SetThesisRegularStyle();
 void SetThesisSmallStyle();
+void SetThumbStyle();
 double ThesisNDCMappingX(double OldX);
 double ThesisNDCMappingY(double OldY);
 double ThesisNDCMappingBigX(double OldX);
@@ -203,6 +204,75 @@ void SetThesisSmallStyle()
    gStyle->SetTitleSize(0.035,"z");
    gStyle->SetTitleOffset(1.10,"x");
    gStyle->SetTitleOffset(1.20,"y");
+   gStyle->SetTitleOffset(1.28,"z");
+
+   gStyle->SetMarkerStyle(20);
+   gStyle->SetHistLineWidth(1);
+   // gStyle->SetHistLineWidth(1.85);
+   // gStyle->SetHistLineWidth(3.85);
+   gStyle->SetLineStyleString(2,"[12 12]"); // postscript dashes
+
+   gStyle->SetErrorX(0.001);
+
+   // gStyle->SetOptTitle(0);
+   gStyle->SetOptStat(0);
+   gStyle->SetOptFit(0);
+
+   gStyle->SetPadTickX(1);
+   gStyle->SetPadTickY(1);
+
+   gStyle->SetOptStat(1100);
+   gStyle->SetOptStat(1);
+   gStyle->SetOptFit(1111);
+   gStyle->SetPadTickX(1);
+   gStyle->SetPadTickY(1);
+
+   gStyle->SetLegendFont(42);
+   gStyle->SetLegendBorderSize(0);
+   gStyle->SetLegendFillColor(0);
+
+   const Int_t NRGBs = 5;
+   const Int_t NCont = 99;
+
+   Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
+   Double_t red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
+   Double_t green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
+   Double_t blue[NRGBs]  = { 0.51, 1.00, 0.12, 0.00, 0.00 };
+   TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
+   gStyle->SetNumberContours(NCont);
+}
+
+void SetThumbStyle()
+{
+   gStyle->SetFrameBorderMode(0);
+   gStyle->SetCanvasBorderMode(0);
+   gStyle->SetPadBorderMode(0);
+   gStyle->SetPadColor(0);
+   gStyle->SetCanvasColor(0);
+   gStyle->SetStatColor(0);
+   gStyle->SetFillColor(0);
+
+   gStyle->SetPaperSize(20,26);
+   gStyle->SetCanvasDefH(1024);
+   gStyle->SetCanvasDefW(1024);
+   gStyle->SetPadTopMargin(0.1);
+   gStyle->SetPadRightMargin(0.1);
+   gStyle->SetPadBottomMargin(0.15);
+   gStyle->SetPadLeftMargin(0.15);
+
+   // gStyle->SetTextFont(42);
+   // gStyle->SetTextSize(0.08);
+   gStyle->SetLabelFont(42,"x");
+   gStyle->SetLabelFont(42,"y");
+   gStyle->SetLabelFont(42,"z");
+   gStyle->SetLabelSize(0.05,"x");
+   gStyle->SetTitleSize(0.06,"x");
+   gStyle->SetLabelSize(0.05,"y");
+   gStyle->SetTitleSize(0.06,"y");
+   gStyle->SetLabelSize(0.05,"z");
+   gStyle->SetTitleSize(0.06,"z");
+   gStyle->SetTitleOffset(1.10,"x");
+   gStyle->SetTitleOffset(1.10,"y");
    gStyle->SetTitleOffset(1.28,"z");
 
    gStyle->SetMarkerStyle(20);
